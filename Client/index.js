2bd2f3673ed343e3d79da67f2402dc5f200baae4
Paul-Louis <prenard@enssat.fr>
2019-05-15 17:01:32 +0200

    Client

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,6 @@
 var express=require('express');                     //Gérez un serveur http
 var parser=require('body-parser');                  //Module pour parser les fichiers json
+var requete=require('./requete.js');
 var app=express();
 app.set('view engine', 'ejs');											//Choix de ejs comme moteur de template
 app.use(express.static(__dirname + '/public'));			//Permet de rendre un répertoire public afin de pouvoir y lire les fichier js et css
@@ -11,6 +12,14 @@ app.use(parser.json());															//Autorise le découpage de json
 *
 **/
 app.get('/',function(req,res){
+  res.render('main');
+});
+
+app.post('/',function(req,res){
+  let chat=req.body.chat;
+  if(chat!=null || chat!=undefined){
+    console.log(chat);
+  }
   res.render('main');
 });
 
