675f011f1ab5e25d096e21caacd7690fc89f0776
Paul-Louis <prenard@enssat.fr>
2019-05-15 16:50:49 +0200

    Client beginning

---
+++ Client/index.js
@@ -0,0 +1,19 @@
+var express=require('express');                     //Gérez un serveur http
+var parser=require('body-parser');                  //Module pour parser les fichiers json
+var app=express();
+app.set('view engine', 'ejs');											//Choix de ejs comme moteur de template
+app.use(express.static(__dirname + '/public'));			//Permet de rendre un répertoire public afin de pouvoir y lire les fichier js et css
+app.use(parser.urlencoded({ extended: true }));			//Autorise le découpage de l'url
+app.use(parser.json());															//Autorise le découpage de json
+
+/**
+* Page principal du site offrant les choix CRUB pour les deux API
+*
+**/
+app.get('/',function(req,res){
+  res.render('main');
+});
+
+
+app.listen(3030);
+console.log("Listening on 3030")
