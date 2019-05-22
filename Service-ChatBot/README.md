889973d1fd70083a4f2f463fcf82ae5421984401
jyra <cmoineau@enssat.fr>
2019-05-22 21:14:42 +0200

    Finalisation de la méthode GET et POST + màj README

--- Service-ChatBot/README.md
+++ Service-ChatBot/README.md
@@ -2,14 +2,57 @@
 
 Run on port 8080 by default.
 
-- [X] Creation of an object robot 
 
-- [ ] Creation of a collection of robot
 
-- [ ] Instanciation of a bot via HTTP request (POST)
+## What this service does : 
 
+- [x] Creation of an object robot 
+- [x] Creation of a collection of robot
+- [x] Instanciation of a bot via HTTP request (POST)
 - [ ] Destruction of a bot via HTTP request (DEL)
-
 - [ ] Modification of a bot via HTTP request (PUT)
+- [x] Getting a list of all the bots via HTTP request (GET)
+- [x] Getting a specific bot via his name
 
-- [ ] Getting a list of all the bots via HTTP request (GET)
+## Random informations 
+
+When you launch this web service a bot is create is name is mockBot and he as the personnality of Steeve (https://github.com/aichaos/rivescript-js/blob/master/eg/brain/begin.rive).
+
+## How to GET the list of bots ?
+
+Simply create a GET request at : http://localhost:8080/
+
+You will receive an answer formated like this :
+
+```JSON
+[{
+  "name": "mockBot",
+  "personality": "steeve",
+  "port": 2020
+}, {
+  "name": "test",
+  "personality": "steeve",
+  "port": 2001
+}]
+```
+
+And if you know the name of a bot and you want details on it do a GET request at : http://localhost:8080/BOT_NAME
+
+You will be served an answer formated like this :
+
+```JSON
+{"name":"mockBot","personality":"steeve","port":2020}
+```
+
+## How to POST a bot on this service ?
+
+You need to create a POST request and send a JSON like this :
+
+```JSon
+{"name" : "test", "personality":"steeve"}
+```
+
+If you send an empty JSON or if informations are lacking the name will be "Anne Onyme" and the personnality "Steeve".
+
+The bot will take a port which isn't currently used starting with the port 2000.
+
--- Service-ChatBot/index.js
+++ Service-ChatBot/index.js
@@ -18,13 +18,17 @@ var listBot = [];
 var bots = [];
 
 bots.push(new bot('mockBot','steeve', 2020));
-listBot.push({})
+listBot.push({
+	name : 'mockBot',
+	personality : 'steeve',
+	port : 2020
+})
 
-function  getBot(index){
-	tmp = 'La tâche d\'id ' + index + ' n\'existe pas ';
-	listBot.forEach( function(bot, i) {
-		if(bot.id == index){
-			tmp =tache;
+function  getBot(name){
+	tmp = null;
+	listBot.forEach(function(bot, i) {
+		if(bot.name == name){
+			tmp = bot;
 		}
 	});
 	return tmp;
@@ -40,25 +44,29 @@ app.get('/', function(req, res){
 	console.log("Demande de la liste des bots")
 	res.json(listBot);
 });
-app.get('/:id', function(req, res){
-	if(req.is('json')){
-		console.log('On màj un bot ');
-		res.json(getBot(req.params.id));
+app.get('/:name', function(req, res){
+	console.log('On demande les informations du bot '+req.params.id );
+	var bot = null;
+	bot = getBot(req.params.name);
+	if(bot != null){
+		res.json(getBot(req.params.name));
 	}
 	else{
-		res.send(400, 'Veuillez envoyer un JSON');
+		res.send('404', 'Bot not found ! ;)');
 	}
 });
 
 // POST
 
 app.post('/', function(req, res){
+	console.log('Demande de création d\'un bot');
 	if(req.is('json')){
 		console.log('On crée un bot');
 		var name = req.body.name;
-		if(name == undefinied) {name = "No name";}
+		if(name == undefined) {name = "Anne Onyme";}
+		console.log('Son nom sera ' + name);
 		var personality = req.body.personality;
-		if(personality == undefinied) {personality = "steeve";}
+		if(personality == undefined) {personality = "steeve";}
 		var flag = true;
 		var port = FIRST_PORT;
 		while(flag){
@@ -66,16 +74,32 @@ app.post('/', function(req, res){
 			listBot.forEach( function(bot, index) {
 				if(bot.port == port){
 					flag = true;
+					port = port +1;
 				}
 			});
-			port = port +1;
+			
 		}
 		bots.push(new bot(name,personality, port));
-		res.send(200, 'Done');
+		listBot.push({
+			name : name,
+			personality : personality,
+			port : port
+		})
+		res.send(200, 'Fait');
 	}
 	else{
 		res.send(400, 'Veuillez envoyer un JSON');
 	}
 });
+
+app.delete('/:name',function(req, res) {
+	console.log("Supprssion du bot "+name);
+    if(undefined!=name){
+		res.send(200,'OK');
+    }else{
+		res.send(404, 'Page introuvable !');
+	}
+});
+
 
 app.listen(port, () => console.log(`Web service running on : http://localhost:${port}`));
